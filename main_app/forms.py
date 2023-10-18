d349fd1f28b43b95475943848fe52474003180c8
Frankie <fwoodwar@gmail.com>
2023-10-18 17:13:46 -0600

    fixed redundancy with character stats

--- main_app/forms.py
+++ main_app/forms.py
@@ -35,7 +35,7 @@ class CharacterForm(ModelForm):
   alignment = forms.ChoiceField(widget=forms.Select)
   class Meta:
       model = Character
-      fields = ['name', 'gender', 'race', 'alignment', 'level', 'exp', 'strength', 'constitution', 'dexterity', 'charisma', 'wisdom', 'intelligence']
+      fields = ['name', 'gender', 'race', 'alignment', 'level', 'exp']
 
   def __init__(self, *args, **kwargs):
       super(CharacterForm, self).__init__(*args, **kwargs)
